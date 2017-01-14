38a488dae6fc0400bc7b72abfea2177c51505281
FilipKernan <filip.kernan@gmail.com>
2017-01-14 09:59:28 -0500

    first commit

---
+++ src/org/frc5459/robot/Robot.java
@@ -0,0 +1,30 @@
+/* Created Sat Jan 14 09:56:00 EST 2017 */
+package org.frc5459.robot;
+
+import org.strongback.Strongback;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+    @Override
+    public void robotInit() {
+    }
+
+    @Override
+    public void teleopInit() {
+        // Start Strongback functions ...
+        Strongback.start();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    @Override
+    public void disabledInit() {
+        // Tell Strongback that the robot is disabled so it can flush and kill commands.
+        Strongback.disable();
+    }
+
+}
---
+++ testsrc/org/frc5459/robot/TestRobot.java
@@ -0,0 +1,13 @@
+/* Created Sat Jan 14 09:56:00 EST 2017 */
+package org.frc5459.robot;
+
+import org.junit.Test;
+
+public class TestRobot {
+
+    @Test
+    public void test() {
+        // do something here
+    }
+
+}
