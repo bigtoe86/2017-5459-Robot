95f16158510d0112dacee4c3516b8c9e46943d05
Owen Dorau <ODorau@gmail.com>
2017-01-15 11:51:24 -0500

    Anitial Bucket Extend and Retract

---
+++ src/org/frc5459/robot/BucketRetractCommand.java
@@ -0,0 +1,21 @@
+package org.frc5459.robot;
+
+import org.strongback.command.Command;
+import org.strongback.components.Solenoid;
+
+public class BucketRetractCommand extends Command {
+	private Solenoid Bucket;
+	
+	
+	
+	public BucketRetractCommand(Solenoid Bucket){
+		this.Bucket = Bucket;
+	}
+	
+	@Override
+	public boolean execute(){
+		Bucket.retract();
+		return true;
+	}
+
+}
--- src/org/frc5459/robot/Robot.java
+++ src/org/frc5459/robot/Robot.java
@@ -2,13 +2,24 @@
 package org.frc5459.robot;
 
 import org.strongback.Strongback;
+import org.strongback.SwitchReactor;
+import org.strongback.components.Solenoid;
+import org.strongback.components.Solenoid.Direction;
+import org.strongback.components.ui.FlightStick;
+import org.strongback.hardware.Hardware;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
+	private FlightStick Operator;
+	private Solenoid Bucket;
+	private SwitchReactor Reactor;
 
     @Override
     public void robotInit() {
+    	Operator = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
+    	Bucket = Hardware.Solenoids.doubleSolenoid(0, 1, Direction.EXTENDING);
+    	Reactor = Strongback.switchReactor();
     }
 
     @Override
@@ -19,6 +30,10 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopPeriodic() {
+    	Reactor.onTriggered(Operator.getButton(5), () -> Strongback.submit(new BucketExtendCommand(Bucket)));
+    	Reactor.onTriggered(Operator.getButton(3), () -> Strongback.submit(new BucketRetractCommand(Bucket)));
+    	
+    	
     }
 
     @Override
