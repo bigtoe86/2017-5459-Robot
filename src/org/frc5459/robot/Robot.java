7c1596465cf307fd6357f4491b78d445c506929e
FilipKernan <filip.kernan@gmail.com>
2017-02-15 10:16:41 -0500

    fixed stupid eclipse things

--- src/org/frc5459/robot/Robot.java
+++ src/org/frc5459/robot/Robot.java
@@ -1,4 +1,4 @@
-/* Created Sat Jan 14 09:56:00 EST 2017 */
+
 package org.frc5459.robot;
 
 import org.strongback.Strongback;
@@ -8,6 +8,7 @@ import org.strongback.components.DistanceSensor;
 import org.strongback.components.Solenoid;
 import org.strongback.components.Solenoid.Direction;
 import org.strongback.components.TalonSRX.FeedbackDevice;
+import org.strongback.components.ui.FlightStick;
 import org.strongback.components.ui.Gamepad;
 import org.strongback.control.TalonController;
 import org.strongback.control.TalonController.ControlMode;
@@ -19,8 +20,8 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 
 public class Robot extends IterativeRobot {
-	private Gamepad driver;
-	private Gamepad operator;
+	private FlightStick driver;
+	private FlightStick operator;
 	private Solenoid bucket;
 	private Solenoid shifter;
 	private SwitchReactor reactor;
@@ -46,46 +47,46 @@ public class Robot extends IterativeRobot {
     @Override
     public void robotInit() {
     	//User Interfaces
-    	driver = Hardware.HumanInterfaceDevices.xbox360(0);
-    	operator = Hardware.HumanInterfaceDevices.xbox360(1);
+    	driver = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
+    	//operator = Hardware.HumanInterfaceDevices.xbox360(1);
     	reactor = Strongback.switchReactor();
-    	
-    	//Manipulator 
-    	bucket = Hardware.Solenoids.doubleSolenoid(0, 1, Direction.EXTENDING);
+//    	
+//    	//Manipulator 
+//    	bucket = Hardware.Solenoids.doubleSolenoid(0, 1, Direction.EXTENDING);
     	//11.37
     	//Motors and Controllers
-    	shifter = Hardware.Solenoids.doubleSolenoid(2, 3, Direction.EXTENDING);
-    	topRight = Hardware.Controllers.talonController(1, 11.37, 0); //TalonSRX #1
-    	middleRight = Hardware.Controllers.talonController(2, 11.37, 0); //TalonSRX #2
-    	bottomRight = Hardware.Controllers.talonController(3, 11.37, 0); //TalonSRX #3
+    	//shifter = Hardware.Solenoids.doubleSolenoid(2, 3, Direction.EXTENDING);
+//    	topRight = Hardware.Controllers.talonController(1, 11.37, 0); //TalonSRX #1
+//    	middleRight = Hardware.Controllers.talonController(2, 11.37, 0); //TalonSRX #2
+//    	bottomRight = Hardware.Controllers.talonController(3, 11.37, 0); //TalonSRX #3
     	climber = Hardware.Controllers.talonController(4, 11.37, 0); //TalonSRX #4
-    	topLeft = Hardware.Controllers.talonController(5, 11.37, 0); //TalonSRX #5
-    	middleLeft = Hardware.Controllers.talonController(6, 11.37,0); //TalonSRX #6
-    	bottomRight = Hardware.Controllers.talonController(7, 11.37,0); //TalonSRX #7
+//    	topLeft = Hardware.Controllers.talonController(5, 11.37, 0); //TalonSRX #5
+//    	middleLeft = Hardware.Controllers.talonController(6, 11.37,0); //TalonSRX #6
+//    	bottomRight = Hardware.Controllers.talonController(7, 11.37,0); //TalonSRX #7
     	climber.reverseOutput(true);
     	
     	//Setting Followers
     	//topRight is Right Side Master (TalonSRX #1)
-    	topRight.withGains(0.062, 0.00062, 0.62);//TODO: make multiple profiles
-    	topRight.setFeedbackDevice(FeedbackDevice.MAGNETIC_ENCODER_ABSOLUTE);
-    	middleRight.setControlMode(ControlMode.FOLLOWER);//TalonSRX #2
-    	middleRight.withTarget(topRight.getDeviceID());
-    	bottomRight.setControlMode(ControlMode.FOLLOWER); //TalonSRX #3
-    	bottomRight.withTarget(topRight.getDeviceID());
+//    	topRight.withGains(0.062, 0.00062, 0.62);//TODO: make multiple profiles
+//    	topRight.setFeedbackDevice(FeedbackDevice.MAGNETIC_ENCODER_ABSOLUTE);
+//    	middleRight.setControlMode(ControlMode.FOLLOWER);//TalonSRX #2
+//    	middleRight.withTarget(topRight.getDeviceID());
+//    	bottomRight.setControlMode(ControlMode.FOLLOWER); //TalonSRX #3
+//    	bottomRight.withTarget(topRight.getDeviceID());
     	//climber is the climber Motor (TalonSRX #4)
     	//TopLeft is Right Side Master (TalonSRX #5)
-    	topLeft.withGains(0.062, 0.00062, 0.62);
-    	topLeft.setFeedbackDevice(FeedbackDevice.MAGNETIC_ENCODER_ABSOLUTE);
-    	topLeft.reverseOutput(true);
-    	middleLeft.setControlMode(ControlMode.FOLLOWER); //TalonSRX #6
-    	middleLeft.withTarget(topLeft.getDeviceID());
-    	bottomLeft.setControlMode(ControlMode.FOLLOWER); //TalonSRX #7
-    	bottomLeft.withTarget(topLeft.getDeviceID());
-    	//Sensors
-    	imu = new ADIS16448IMU();
+//    	topLeft.withGains(0.062, 0.00062, 0.62);
+//    	topLeft.setFeedbackDevice(FeedbackDevice.MAGNETIC_ENCODER_ABSOLUTE);
+//    	topLeft.reverseOutput(true);
+//    	middleLeft.setControlMode(ControlMode.FOLLOWER); //TalonSRX #6
+//    	middleLeft.withTarget(topLeft.getDeviceID());
+//    	bottomLeft.setControlMode(ControlMode.FOLLOWER); //TalonSRX #7
+//    	bottomLeft.withTarget(topLeft.getDeviceID());
+//    	//Sensors
+    	//imu = new ADIS16448IMU();
     	//drive
-    	drive = new Drive5459(topRight, topLeft, ultraX, ultraY, imu, shifter);
-    	dataBase = NetworkTable.getTable("SmartDashboard");
+//    	drive = new Drive5459(topRight, topLeft, ultraX, ultraY, null, shifter);
+//    	dataBase = NetworkTable.getTable("SmartDashboard");
 
   
     }   
@@ -108,25 +109,26 @@ public class Robot extends IterativeRobot {
     
     @Override
     public void teleopInit() {
-        Strongback.submit(new TeleopDriveCommand(drive, driver));
+        //Strongback.submit(new TeleopDriveCommand(drive, driver));
+    	Strongback.start();
     }
 
     @Override
     public void teleopPeriodic() {    	
-    	if (operator.getRightTrigger().read() > 0.5) {
-    		Strongback.submit(new BucketExtendCommand(bucket));
-		}else if( operator.getLeftTrigger().read() > 0.5){
-			Strongback.submit(new BucketRetractCommand(bucket));
-		}
+//    	if (operator.getRightTrigger().read() > 0.5) {
+//    		Strongback.submit(new BucketExtendCommand(bucket));
+//		}else if( operator.getLeftTrigger().read() > 0.5){
+//			Strongback.submit(new BucketRetractCommand(bucket));
+//		}
 
-    	reactor.whileTriggered(operator.getRightBumper(), () -> Strongback.submit(new AscendClimbCommand(climber)));
-    	reactor.whileUntriggered(operator.getRightBumper(), () -> Strongback.submit(new StopClimbCommand(climber)));
-    	distance = dataBase.getNumber("Distance", 0.0);
-    	horizontalDistance = dataBase.getNumber("horizontalDistance", 0.0);
-    	rotationalAngle = dataBase.getNumber("rotationAngle", 0.0);
-    	System.out.println(distance);
-    	System.out.println(horizontalDistance);
-    	System.out.println(rotationalAngle);
+    	reactor.whileTriggered(driver.getTrigger(), () -> Strongback.submit(new AscendClimbCommand(climber)));
+    	reactor.whileUntriggered(driver.getTrigger(), () -> Strongback.submit(new StopClimbCommand(climber)));
+//    	distance = dataBase.getNumber("Distance", 0.0);
+//    	horizontalDistance = dataBase.getNumber("horizontalDistance", 0.0);
+//    	rotationalAngle = dataBase.getNumber("rotationAngle", 0.0);
+//    	System.out.println(distance);
+//    	System.out.println(horizontalDistance);
+//    	System.out.println(rotationalAngle);
     }
 
     @Override
--- testsrc/org/frc5459/robot/TestRobot.java
+++ testsrc/org/frc5459/robot/TestRobot.java
@@ -13,16 +13,16 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.strongback.mock.Mock;
 
-import org.strongback.mock.MockGyroscope;
-import org.strongback.mock.MockSolenoid;
+//import org.strongback.mock.MockGyroscope;
+//import org.strongback.mock.MockSolenoid;
 
 
-import org.strongback.mock.MockMotor;
-import org.strongback.mock.MockSolenoid;
+//import org.strongback.mock.MockMotor;
+//import org.strongback.mock.MockSolenoid;
 
 
 public class TestRobot {
-	private MockMotor motor;
+	//private MockMotor motor;
 	//for AscendClimbCommand
 //    @Test
 //    @Ignore
@@ -60,75 +60,75 @@ public class TestRobot {
 //        System.out.println("shouldStopWhenStopped passed");
 //    }
 
-	@Ignore
-	@Test
-	public void shouldExtendWhenAlreadyExtended(){
-	    MockSolenoid solenoid = Mock.manualSolenoid().extend();
-	    System.out.println("Solenoid created");
-	    BucketExtendCommand command = new BucketExtendCommand(solenoid);
-	    System.out.println("Command created");
-	    assertThat(solenoid.isExtending()).isTrue();
-	    assertThat(solenoid.isRetracting()).isFalse();
-	    assertThat(command.execute()).isTrue();
-	    solenoid.stop();
-	    assertThat(solenoid.isStopped()).isTrue();
-	    System.out.println("shouldExtendWhenAlreadyExtended passed");
-	}
-	@Ignore
-	@Test
-	public void shouldExtendWhenRetracted(){
-        MockSolenoid solenoid = Mock.manualSolenoid().retract();
-        System.out.println("Solenoid created");
-        BucketExtendCommand command = new BucketExtendCommand(solenoid);
-        System.out.println("Command created");
-        assertThat(solenoid.isExtending()).isTrue();
-        assertThat(solenoid.isRetracting()).isFalse();
-        assertThat(command.execute()).isTrue();
-        solenoid.stop();
-        assertThat(solenoid.isStopped()).isTrue();   
-        System.out.println("shouldExtendWhenRetracted passed");
-	}
-	@Ignore
-	@Test
-	public void shouldRetractWhenAlreadyExtended(){
-	    MockSolenoid solenoid = Mock.manualSolenoid().extend();
-	    System.out.println("Solenoid created");
-	    BucketRetractCommand command = new BucketRetractCommand(solenoid);
-	    System.out.println("Command created");
-	    assertThat(command.execute()).isTrue();
-	    System.out.println(solenoid.isRetracting());
-	    assertThat(solenoid.isRetracting()).isTrue();
-	    assertThat(solenoid.isExtending()).isFalse();
-	    
-	    
-	    solenoid.stop();
-	    assertThat(solenoid.isStopped()).isTrue();
-	    System.out.println("shouldRetractWhenAlreadyExtended passed");
-	}
-	@Ignore
-	@Test
-	public void shouldRetractWhenRetracted(){
-        MockSolenoid solenoid = Mock.manualSolenoid().retract();
-        System.out.println("Solenoid created");
-        BucketRetractCommand command = new BucketRetractCommand(solenoid);
-        System.out.println("Command created");
-        solenoid.retract();
-        assertThat(command.execute()).isTrue();
-        System.out.println(solenoid.isRetracting());
-        assertThat(solenoid.isExtending()).isFalse();
-        assertThat(solenoid.isRetracting()).isTrue();
-        
-        solenoid.stop();
-        assertThat(solenoid.isStopped()).isTrue();   
-        System.out.println("shouldRetractWhenRetracted passed");
-	}
-	@Test
-	@Ignore
-	public void Drive5459test(){
-		MockGyroscope gyro;
-	//	MockTalonController right;
-	//	Drive5459 Test = new Drive5459();
-	}
+//	@Ignore
+//	@Test
+//	public void shouldExtendWhenAlreadyExtended(){
+//	    MockSolenoid solenoid = Mock.manualSolenoid().extend();
+//	    System.out.println("Solenoid created");
+//	    BucketExtendCommand command = new BucketExtendCommand(solenoid);
+//	    System.out.println("Command created");
+//	    assertThat(solenoid.isExtending()).isTrue();
+//	    assertThat(solenoid.isRetracting()).isFalse();
+//	    assertThat(command.execute()).isTrue();
+//	    solenoid.stop();
+//	    assertThat(solenoid.isStopped()).isTrue();
+//	    System.out.println("shouldExtendWhenAlreadyExtended passed");
+//	}
+//	@Ignore
+//	@Test
+//	public void shouldExtendWhenRetracted(){
+//        MockSolenoid solenoid = Mock.manualSolenoid().retract();
+//        System.out.println("Solenoid created");
+//        BucketExtendCommand command = new BucketExtendCommand(solenoid);
+//        System.out.println("Command created");
+//        assertThat(solenoid.isExtending()).isTrue();
+//        assertThat(solenoid.isRetracting()).isFalse();
+//        assertThat(command.execute()).isTrue();
+//        solenoid.stop();
+//        assertThat(solenoid.isStopped()).isTrue();   
+//        System.out.println("shouldExtendWhenRetracted passed");
+//	}
+//	@Ignore
+//	@Test
+//	public void shouldRetractWhenAlreadyExtended(){
+//	    MockSolenoid solenoid = Mock.manualSolenoid().extend();
+//	    System.out.println("Solenoid created");
+//	    BucketRetractCommand command = new BucketRetractCommand(solenoid);
+//	    System.out.println("Command created");
+//	    assertThat(command.execute()).isTrue();
+//	    System.out.println(solenoid.isRetracting());
+//	    assertThat(solenoid.isRetracting()).isTrue();
+//	    assertThat(solenoid.isExtending()).isFalse();
+//	    
+//	    
+//	    solenoid.stop();
+//	    assertThat(solenoid.isStopped()).isTrue();
+//	    System.out.println("shouldRetractWhenAlreadyExtended passed");
+//	}
+//	@Ignore
+//	@Test
+//	public void shouldRetractWhenRetracted(){
+//        MockSolenoid solenoid = Mock.manualSolenoid().retract();
+//        System.out.println("Solenoid created");
+//        BucketRetractCommand command = new BucketRetractCommand(solenoid);
+//        System.out.println("Command created");
+//        solenoid.retract();
+//        assertThat(command.execute()).isTrue();
+//        System.out.println(solenoid.isRetracting());
+//        assertThat(solenoid.isExtending()).isFalse();
+//        assertThat(solenoid.isRetracting()).isTrue();
+//        
+//        solenoid.stop();
+//        assertThat(solenoid.isStopped()).isTrue();   
+//        System.out.println("shouldRetractWhenRetracted passed");
+//	}
+//	@Test
+//	@Ignore
+//	public void Drive5459test(){
+//		MockGyroscope gyro;
+//	//	MockTalonController right;
+//	//	Drive5459 Test = new Drive5459();
+//	}
     	
 	
 }
