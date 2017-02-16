443d3d24a235a5262bb70168f268436c760e5afc
FilipKernan <filip.kernan@gmail.com>
2017-02-15 22:45:24 -0500

    fixed eclipes errors

--- src/org/frc5459/robot/Robot.java
+++ src/org/frc5459/robot/Robot.java
@@ -13,15 +13,14 @@ import org.strongback.components.ui.Gamepad;
 import org.strongback.control.TalonController;
 import org.strongback.control.TalonController.ControlMode;
 import org.strongback.hardware.Hardware;
-import org.frc5459.robot.BucketExtendCommand;
-import org.frc5459.robot.BucketRetractCommand;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 
 public class Robot extends IterativeRobot {
-	private FlightStick driver;
-	private FlightStick operator;
+	private Gamepad driver;
+	private Gamepad operator;
 	private Solenoid bucket;
 	private Solenoid shifter;
 	private SwitchReactor reactor;
@@ -47,46 +46,47 @@ public class Robot extends IterativeRobot {
     @Override
     public void robotInit() {
     	//User Interfaces
-    	driver = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
+    	driver = Hardware.HumanInterfaceDevices.xbox360(0);
     	//operator = Hardware.HumanInterfaceDevices.xbox360(1);
     	reactor = Strongback.switchReactor();
-//    	
-//    	//Manipulator 
-//    	bucket = Hardware.Solenoids.doubleSolenoid(0, 1, Direction.EXTENDING);
-    	//11.37
+    	
+    	//Manipulator 
+    	bucket = Hardware.Solenoids.doubleSolenoid(0, 1, Direction.RETRACTING);
+    	System.out.println("hi");
     	//Motors and Controllers
-    	//shifter = Hardware.Solenoids.doubleSolenoid(2, 3, Direction.EXTENDING);
-//    	topRight = Hardware.Controllers.talonController(1, 11.37, 0); //TalonSRX #1
-//    	middleRight = Hardware.Controllers.talonController(2, 11.37, 0); //TalonSRX #2
-//    	bottomRight = Hardware.Controllers.talonController(3, 11.37, 0); //TalonSRX #3
+    	shifter = Hardware.Solenoids.doubleSolenoid(2, 3, Direction.EXTENDING);
+    	topRight = Hardware.Controllers.talonController(1, 11.37, 0); //TalonSRX #1
+    	middleRight = Hardware.Controllers.talonController(2, 11.37, 0); //TalonSRX #2
+    	bottomRight = Hardware.Controllers.talonController(3, 11.37, 0); //TalonSRX #3
     	climber = Hardware.Controllers.talonController(4, 11.37, 0); //TalonSRX #4
-//    	topLeft = Hardware.Controllers.talonController(5, 11.37, 0); //TalonSRX #5
-//    	middleLeft = Hardware.Controllers.talonController(6, 11.37,0); //TalonSRX #6
-//    	bottomRight = Hardware.Controllers.talonController(7, 11.37,0); //TalonSRX #7
+    	topLeft = Hardware.Controllers.talonController(5, 11.37, 0); //TalonSRX #5
+    	middleLeft = Hardware.Controllers.talonController(6, 11.37,0); //TalonSRX #6
+    	bottomRight = Hardware.Controllers.talonController(7, 11.37,0); //TalonSRX #7
     	climber.reverseOutput(true);
+    	System.out.println("hello");
     	
     	//Setting Followers
     	//topRight is Right Side Master (TalonSRX #1)
-//    	topRight.withGains(0.062, 0.00062, 0.62);//TODO: make multiple profiles
-//    	topRight.setFeedbackDevice(FeedbackDevice.MAGNETIC_ENCODER_ABSOLUTE);
-//    	middleRight.setControlMode(ControlMode.FOLLOWER);//TalonSRX #2
-//    	middleRight.withTarget(topRight.getDeviceID());
-//    	bottomRight.setControlMode(ControlMode.FOLLOWER); //TalonSRX #3
-//    	bottomRight.withTarget(topRight.getDeviceID());
+    	topRight.withGains(0.062, 0.00062, 0.62);//TODO: make multiple profiles
+    	topRight.setFeedbackDevice(FeedbackDevice.MAGNETIC_ENCODER_ABSOLUTE);
+    	middleRight.setControlMode(ControlMode.FOLLOWER);//TalonSRX #2
+    	middleRight.withTarget(topRight.getDeviceID());
+    	bottomRight.setControlMode(ControlMode.FOLLOWER); //TalonSRX #3
+    	bottomRight.withTarget(topRight.getDeviceID());
     	//climber is the climber Motor (TalonSRX #4)
     	//TopLeft is Right Side Master (TalonSRX #5)
-//    	topLeft.withGains(0.062, 0.00062, 0.62);
-//    	topLeft.setFeedbackDevice(FeedbackDevice.MAGNETIC_ENCODER_ABSOLUTE);
-//    	topLeft.reverseOutput(true);
-//    	middleLeft.setControlMode(ControlMode.FOLLOWER); //TalonSRX #6
-//    	middleLeft.withTarget(topLeft.getDeviceID());
-//    	bottomLeft.setControlMode(ControlMode.FOLLOWER); //TalonSRX #7
-//    	bottomLeft.withTarget(topLeft.getDeviceID());
-//    	//Sensors
+    	topLeft.withGains(0.062, 0.00062, 0.62);
+    	topLeft.setFeedbackDevice(FeedbackDevice.MAGNETIC_ENCODER_ABSOLUTE);
+    	topLeft.reverseOutput(true);
+    	middleLeft.setControlMode(ControlMode.FOLLOWER); //TalonSRX #6
+    	middleLeft.withTarget(topLeft.getDeviceID());
+    	//bottomLeft.setControlMode(ControlMode.FOLLOWER); //TalonSRX #7
+    	//bottomLeft.withTarget(topLeft.getDeviceID());
+    	//Sensors
     	//imu = new ADIS16448IMU();
     	//drive
-//    	drive = new Drive5459(topRight, topLeft, ultraX, ultraY, null, shifter);
-//    	dataBase = NetworkTable.getTable("SmartDashboard");
+    	drive = new Drive5459(topRight, topLeft, ultraX, ultraY, null, shifter);
+    	//dataBase = NetworkTable.getTable("DataBase");
 
   
     }   
@@ -109,26 +109,31 @@ public class Robot extends IterativeRobot {
     
     @Override
     public void teleopInit() {
-        //Strongback.submit(new TeleopDriveCommand(drive, driver));
     	Strongback.start();
+    	//Strongback.submit(new TeleopDriveCommand(drive, driver));
     }
 
     @Override
     public void teleopPeriodic() {    	
-//    	if (operator.getRightTrigger().read() > 0.5) {
-//    		Strongback.submit(new BucketExtendCommand(bucket));
-//		}else if( operator.getLeftTrigger().read() > 0.5){
-//			Strongback.submit(new BucketRetractCommand(bucket));
-//		}
+    	if (driver.getRightBumper().isTriggered()) {
+    		//Strongback.submit(new BucketExtendCommand(bucket));
+    		bucket.extend();
+		}else if( driver.getLeftBumper().isTriggered()){
+			//Strongback.submit(new BucketRetractCommand(bucket));
+			bucket.retract();
+		}
 
-    	reactor.whileTriggered(driver.getTrigger(), () -> Strongback.submit(new AscendClimbCommand(climber)));
-    	reactor.whileUntriggered(driver.getTrigger(), () -> Strongback.submit(new StopClimbCommand(climber)));
+    	//reactor.whileTriggered(driver.getRightBumper(), () -> Strongback.submit(new AscendClimbCommand(climber)));
+    	//reactor.whileUntriggered(driver.getRightBumper(), () -> Strongback.submit(new StopClimbCommand(climber)));
 //    	distance = dataBase.getNumber("Distance", 0.0);
 //    	horizontalDistance = dataBase.getNumber("horizontalDistance", 0.0);
 //    	rotationalAngle = dataBase.getNumber("rotationAngle", 0.0);
-//    	System.out.println(distance);
-//    	System.out.println(horizontalDistance);
-//    	System.out.println(rotationalAngle);
+//    	double angle = dataBase.getNumber("angle", 0);
+//    	System.out.println("The distance to the target is" + distance);
+//    	System.out.println("The horizontal distance is " + horizontalDistance);
+//    	System.out.println("The first angle is " + rotationalAngle + ".  The second one (based on dis) is " + angle);
+//    	//TODO: test the drive train today and try to get raspi done as well
+    	Timer.delay(0.05);
     }
 
     @Override
